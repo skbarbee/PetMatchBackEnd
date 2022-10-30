aa4bf56aeeb9cde73c5439ac7f59955e8cbd6e1a
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-10-30 10:54:35 -0500

    added petModel in the backend

---
+++ app/models/petModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const petSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Pet', petSchema)
