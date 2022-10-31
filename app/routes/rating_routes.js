ecaae298480fd01fb916fba74d065b1925a87671
jlm8965 <JulieLMonsees@gmail.com>
2022-10-31 03:05:02 -0500

    Rating Route and Font Awesome library icons added

---
+++ app/routes/rating_routes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+//pulling in Mongoose model for ratings
+const Rating = require('..models/rating')
+const router = expres.Router()
+
+//POST -> anybody can leave a rating for a pet (For now)
+router.post('/rating/:petId', removeBlanks, (req, res, next) => {
+    // get the rating from req.body
+    const rating = req.body.rating
+    const petId = req.params.petId
+    // find the pet by its id
+    Pet.findById(petId)
+        .then(handle404)
+        // add the rating to the pet
+        .then(pet => {
+            // push the rating into the pet's rating array and return the saved pet
+            pet.rating.push(rating)
+
+            return pet.save()
+        })
+        .then(pet => res.status(201).json({ pet: pet }))
+        // pass to the next thing
+        .catch(next)
+})
