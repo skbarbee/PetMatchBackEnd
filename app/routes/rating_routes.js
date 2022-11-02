e4083b171a466cf85216fce9d3a87b34c8bcb1b9
jlm8965 <JulieLMonsees@gmail.com>
2022-11-02 17:18:42 -0500

    Rating troubleshoot

--- app/routes/rating_routes.js
+++ app/routes/rating_routes.js
@@ -22,10 +22,10 @@ const router = express.Router()
 
 //pulling in Mongoose model for ratings
 // const Rating = require('..models/rating')
-const Pet = require('../models/petModel')
-const router = express.Router()
+// const Pet = require('../models/petModel')
+// const router = express.Router()
 
-const removeBlanks = require('../../lib/remove_blank_fields')
+// const removeBlanks = require('../../lib/remove_blank_fields')
 
 //POST -> anybody can leave a rating for a pet (For now)
 router.post('/rating/:petId', removeBlanks, (req, res, next) => {
