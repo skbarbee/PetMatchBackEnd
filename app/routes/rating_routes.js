411e76fcc7e7e39749a46081b3bf5cf851edf09e
skbarbee <barbee.s.k@gmail.com>
2022-11-02 18:14:43 -0400

    exported routes

--- app/routes/rating_routes.js
+++ app/routes/rating_routes.js
@@ -30,6 +30,7 @@ router.post('/rating/:petId', removeBlanks, (req, res, next) => {
     const petId = req.params.petId
     // find the pet by its id
     Pet.findById(petId)
+        .then((pet)=>{console.log({pet: pet})})
         .then(handle404)
         // add the rating to the pet
         .then(pet => {
@@ -42,3 +43,5 @@ router.post('/rating/:petId', removeBlanks, (req, res, next) => {
         // pass to the next thing
         .catch(next)
 })
+
+module.exports = router
