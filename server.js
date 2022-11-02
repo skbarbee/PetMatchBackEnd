766edf3885a36767bc06ee35d755831362e813eb
jlm8965 <JulieLMonsees@gmail.com>
2022-11-02 02:33:21 -0500

    rating added

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.use(requestLogger)
 // app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(petRoutes)
+app.use(ratingRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
