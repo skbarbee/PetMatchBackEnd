6803ca1b95fd193ec9adba8ade0cc1101c0f7199
skbarbee <barbee.s.k@gmail.com>
2022-11-03 15:25:19 -0400

    cloudinary works

--- app/routes/image_routes.js
+++ app/routes/image_routes.js
@@ -14,17 +14,18 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 
-router.post('/image/:petId',removeBlanks, async (req, res, next)=>{
+router.post('/image/:petId',removeBlanks,  (req, res, next)=>{
 	try{
 		const fileStr = req.body.image
 
 		//console.log('this is the file\n', fileStr) 
     	const petId = req.params.petId
-		const uploadedResponse = await cloudinary.uploader.upload_large(fileStr,{
-			width: 300,
-		})
-		console.log('this is the uploaded info\n', uploadedResponse)
-		Pet.findByIdAndUpdate(petId,  {img: uploadedResponse.url} ,
+		console.log('this is fileStr in imagePost', fileStr)
+		// const uploadedResponse = await cloudinary.uploader.upload_large(fileStr,{
+		// 	width: 300,
+		// })
+		// console.log('this is the uploaded info\n', uploadedResponse)
+		Pet.findByIdAndUpdate(petId,  {img: fileStr} ,
 			function (err, doc) {
 					if (err){
 					console.log(err)
