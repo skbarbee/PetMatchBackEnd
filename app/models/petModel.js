30ced019569e6ae9091cbdeb5d27b384cca7e698
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-11-04 16:10:33 -0500

    almost got the messages to show

--- app/models/petModel.js
+++ app/models/petModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const meetSchema = require('./meet')
 const ratingSchema = require('./rating')
-
+const petMessageSchema = require('./petMessage')
 
 
 const petSchema = new mongoose.Schema(
@@ -31,17 +31,13 @@ const petSchema = new mongoose.Schema(
 			required: false,
 		},
 		rating: [ratingSchema],
+		meets: [meetSchema],
+		petMessages: [petMessageSchema],
 		owner: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
 			required: true,
 		},
-		meets: [meetSchema],
-		owner: {
-			type: mongoose.Schema.Types.ObjectId,
-        	ref:'User',
-		},
-		
 	},
 	{
 		timestamps: true,
---
+++ app/routes/petMessage_routes.js
@@ -0,0 +1,104 @@
+// basic subdoc routes, like the comments in the fruit app
+
+const express = require('express')
+const passport = require('passport')
+
+const Pet = require('../models/petModel')
+
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+const router = express.Router()
+
+// only need Create Update and Delete
+
+// POST -> anybody can leave a pet a message
+// POST /petMessages/<pet_id>
+router.post('/petMessages/:petId', removeBlanks, (req, res, next) => {
+    // get the petMessage from req.body
+    const petMessages= req.body.petMessages
+    const petId = req.params.petId
+    // find the pet by it's ID
+    Pet.findByIdAndUpdate(petId)
+        .then(handle404)
+        // add the petMessage to the pet
+        .then(pet => {
+            // push the petMessage into the pet's petMessage array and retunr the saved pet
+            pet.petMessages.push(petMessages)
+            console.log("this is the pet", pet)
+            return pet.save()
+        })
+        .then(pet => res.status(201).json({ pet: pet }))
+        .catch(next)
+        // pass to the next thing
+})
+
+// Sarah and Emily tried below route to get messages to post.
+// router.post('/petMessages/:petId', removeBlanks, (req, res, next) => {
+//     try{
+//     const petMessages = req.body.petMessages 
+//     const petId = req.params.petId
+
+//     Pet.findByIdAndUpdate(petId,  {petMessages: petMessages} ,
+//         function (err, doc) {
+//                 if (err){
+//                 console.log(err)
+//                 }
+//                 else{
+//                 console.log("Updated Pet : ", doc);
+//                 }
+//                 })
+//     res.json({msg: "success"})
+//     } catch (error){
+//         console.log(error)
+//         res.status(500).json({err:error})
+//     }
+// })
+
+
+// UPDATE a petMessage
+// PATCH -> /petMessages/<pet_id>//<petMessage_id>
+router.patch('/petMessages/:petId/:petMessageId', requireToken, removeBlanks, (req, res, next) => {
+    const { petId, petMessageId } = req.params
+
+    Pet.findById(petId)
+        .then(handle404)
+        .then(pet => {
+            const thePetMessage = pet.petMessages.id(petMessageId)
+
+            requireOwnership(req, pet)
+
+            thePetMessage.set(req.body.petMessage)
+
+            return pet.save()
+
+        })
+        .then(pet => res.sendStatus(204))
+        .catch(next)
+})
+
+
+// DESTROY a petMessage
+// DELETE -> /petMessages/<pet_id>//<petMessage_id>
+router.delete('/petMessages/:petId/:petMessageId', requireToken, (req, res, next) => {
+    const { petId, petMessageId } = req.params
+
+    Pet.findById(petId)
+        .then(handle404)
+        .then(pet => {
+            const thePetMessage = pet.petMessages.id(petMessageId)
+
+            requireOwnership(req, pet)
+
+            thePetMessage.remove()
+
+            return pet.save()
+        })
+        .then(pet => res.sendStatus(204))
+        .catch(next)
+})
+
+// export router
+module.exports = router
